55d5b7a8906909464151718bf9a58c7c7455b25f
Olena Boldysheva <adv55577@gmail.com>
2022-09-06 15:17:05 +0300

    add change password form

--- frontend/src/components/settings/ChangePasswordForm.tsx
+++ frontend/src/components/settings/ChangePasswordForm.tsx
@@ -1,33 +1,82 @@
-import React from 'react';
-import { Input } from 'components/common/inputs/Input';
-import { Typography } from 'components/common/Typography';
+import React from 'react'
+import { Typography } from 'components/common/Typography'
+import { Field, Form, Formik, FormikProps } from 'formik'
+import { Button } from 'components/common/buttons/Button'
+import { PasswordInput } from 'components/common/inputs/PasswordInput'
+import { changePasswordSchema } from './schemas/changePasswordSchema'
+
+interface ChangePasswordFormProps {
+  new_password: string
+  confirm_password: string
+}
+
+const InitialValues: ChangePasswordFormProps = {
+  new_password: '',
+  confirm_password: '',
+}
 
 const ChangePasswordForm: React.FC = () => {
+  const handleSubmit = (values: typeof InitialValues) => {
+    console.log(values)
+  }
   return (
-    <>
-      <Typography type={'h4'} children={'Change Password'} />
-      <div className="flex flex-col gap-5 sm:flex-row">
-        <div className="sm:w-1/2">
-          <Input
-            label={'New Password'}
-            type={'password'}
-            name={'new-password-input'}
-            placeholder={'e.g. *******'}
-            // onChange={(e) => console.log(e.target.value)}
-          />
-        </div>
-        <div className="sm:w-1/2">
-          <Input
-            label={'Confirm Password'}
-            type={'password'}
-            name={'confirm-password-input'}
-            placeholder={'e.g. *******'}
-            // onChange={(e) => console.log(e.target.value)}
-          />
-        </div>
-      </div>
-    </>
-  );
-};
+    <Formik
+      initialValues={InitialValues}
+      validateOnBlur={true}
+      validateOnChange={true}
+      validationSchema={changePasswordSchema}
+      onSubmit={handleSubmit}
+    >
+      {(props: FormikProps<ChangePasswordFormProps>) => {
+        const { dirty, isValid, errors, handleBlur, handleChange, values } =
+          props
+        return (
+          <>
+            <Typography type={'h4'}>{'Change Password'}</Typography>
+            <Form className="flex flex-col gap-8 mb-5">
+              <div className="flex flex-col gap-5 sm:flex-row">
+                <Field
+                  label={'New Password'}
+                  name={'new_password'}
+                  id={'new_password'}
+                  placeholder={'e.g. *******'}
+                  values={values.new_password}
+                  error={errors.new_password}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  component={PasswordInput}
+                  className="sm:w-1/2"
+                />
 
-export default ChangePasswordForm;
+                <Field
+                  label={'Confirm Password'}
+                  name={'confirm_password'}
+                  id={'confirm_password'}
+                  placeholder={'e.g. *******'}
+                  values={values.confirm_password}
+                  error={errors.confirm_password}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  className="sm:w-1/2"
+                  component={PasswordInput}
+                />
+              </div>
+              <div className="flex justify-center lg:justify-end">
+                <div className="w-full lg:w-1/2">
+                  <Button
+                    label={'Change Password'}
+                    type={'submit'}
+                    btnName={'primary'}
+                    disabled={!(isValid && dirty)}
+                  />
+                </div>
+              </div>
+            </Form>
+          </>
+        )
+      }}
+    </Formik>
+  )
+}
+
+export default ChangePasswordForm
