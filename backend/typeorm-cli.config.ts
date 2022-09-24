4a1ba47b42ab63af11efaab55568c552f8e466df
Serhey Hlushak <glushak_sergey@i.ua>
2022-09-24 21:47:28 +0300

    typeorm client settings in typeorm-cli.config.ts commented out

--- backend/typeorm-cli.config.ts
+++ backend/typeorm-cli.config.ts
@@ -1,7 +1,5 @@
 import { DataSource } from 'typeorm';
 
-// require('dotenv').config();
-
 export default new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
@@ -9,6 +7,20 @@ export default new DataSource({
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  entities: [__dirname + '/**/*.entity{.js}'],
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/src/migrations/*{.ts,.js}'],
 });
+
+// require('dotenv').config();
+
+// export const PostgresDataSource = new DataSource({
+//     type: 'postgres',
+//     host: process.env.DB_HOST,
+//     port: parseInt(process.env.DB_PORT, 10),
+//     username: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DB,
+//     synchronize: false,
+//     entities: ['dist/**/*.entity.js'],
+//     migrations: ["src/migrations/**/*.ts"],
+// })
